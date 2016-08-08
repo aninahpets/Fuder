13a384b0e38420e0ea535a6bb556b4e4b5125018
Nina <smulhollandfuchs@gmail.com>
2016-08-08 23:29:58 +0000

    Added yelp search route and added framework for cookie sessions

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.model):
+class User(db.Model):
     """User of the app."""
 
     __tablename__ = 'users'
@@ -13,37 +13,37 @@ class User(db.model):
     password = db.Column(db.String(75), nullable=False)
 
 
-class Venue(db.model):
+class Venue(db.Model):
     """Restaurant that a user has visited through the app."""
 
     __tablename__ = 'venues'
 
     venue_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    latitude = db.Column(db.Decimal(10,6), nullable=False)
-    longitude = db.Column(db.Decimal(10,6), nullable=False)
+    latitude = db.Column(db.Float(10,6), nullable=False)
+    longitude = db.Column(db.Float(10,6), nullable=False)
 
 
-class Visit(db.model):
+class Visit(db.Model):
     """Visit that a user has made to a venue."""
 
     __tablename__ = 'visits'
 
     visit_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'),
-        nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey(venues.venue_id),
-        nullable=False)
-    visited_at = db.Column(db.Integer, db.DateTime,
-        default=datetime.datetime.utcnow)
+                        nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.venue_id'),
+                        nullable=False)
+    visited_at = db.Column(db.DateTime,
+                         default=datetime.utcnow())
 
     #Define relationship to user
-    user = db.relationship('User', backref=db.backref('visits',
-        order_by=visit_id))
+    user = db.relationship('User',
+                            backref=db.backref('visits', order_by=visit_id))
 
     #Define relationship to venue
-    venue = db.relationship('Venue', backref=db.backref('visits',
-        order_by=visit_id))
+    venue = db.relationship('Venue',
+                            backref=db.backref('visits', order_by=visit_id))
 
 
 
@@ -52,7 +52,8 @@ class Visit(db.model):
 def connect_to_db(app):
     """Connect the database to the Flask app."""
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///restaurantrides'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///project'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
 
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import (Flask, render_template, redirect, request, session)
+import os
+from flask import (Flask, render_template, redirect, request, flash, session)
 from model import User, Venue, Visit, connect_to_db, db
 from flask_sqlalchemy import SQLAlchemy
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
 
 app = Flask(__name__)
 
@@ -8,15 +11,86 @@ app = Flask(__name__)
 def index():
     """Homepage"""
 
-    return render_template('index.html')
+    # checks to see if user logged in
+    if 'user_id' in session:
+        return render_template('index.html')
+    else:
+        flash('Please log in.')
+        return redirect('/login')
+
+@app.route('/login')
+def login():
+    """Provides user login"""
+
+    return render_template('login.html')
+
+@app.route('/yelp_search')
+def search_yelp():
+    # using Python os.environ to access environment variables
+    yelp_auth = Oauth1Authenticator(
+        consumer_key=os.environ['yelp_consumer_key'],
+        consumer_secret=os.environ['yelp_consumer_secret'],
+        token=os.environ['yelp_token'],
+        token_secret=os.environ['yelp_token_secret'])
+
+    location = request.args.get('user-address')
+
+    params = {'term': 'restaurant', 'location': location}
+
+    client = Client(yelp_auth)
+    result = client.search(params)
+    return result
+
+
+@app.route('/submit', methods=['POST'])
+def submit_login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # checks for existing user in database
+    user = User.query.filter_by(email=email).one()
+    user_id = user.user_id
+
+    # checks to see if another user is already logged in and logs them out if so
+    if 'user_id' in session:
+        del session['user_id']
+
+        # checks for password match
+        if user.password == password:
+            return redirect('/')
+        else:
+            flash('Your password was incorrect. Please try again.')
+            return redirect('/login')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form.get('new-email')
+    password = request.form.get('new-password')
+    
 
 @app.route('/request')
+def request():
     """User view while Uber ride request is processing"""
 
     return render_template('processing.html')
 
 
 @app.route('/history')
+def history():
     """User view of their complete venue visit history"""
 
     return render_template('visit-history.html')
+
+
+
+if __name__ == '__main__':
+   
+    app.debug = True
+   
+    connect_to_db(app)
+   
+    # DebugToolbarExtension(app)
+
+    app.run()
+

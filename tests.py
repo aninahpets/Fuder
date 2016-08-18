9722f64a1173965767e21dde53d8796229f197b3
Nina <smulhollandfuchs@gmail.com>
2016-08-18 16:31:27 -0700

    Improved Selenium tests and added additional tests; restructured test classes

--- tests.py
+++ tests.py
@@ -2,24 +2,58 @@ import unittest
 from selenium import webdriver
 
 from server import app
-from model import db, connect_to_db
+from model import db, connect_to_db, example_data
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///project"
-
-class HomepageTests(unittest.TestCase):
+class NotLoggedInTests(unittest.TestCase):
     """Tests for non-registered users."""
 
     def setUp(self):
-        self.client = app.test_client()
         app.config['TESTING'] = True
+        self.client = app.test_client()
+
+    def test_registration_page(self):
+        result = self.client.get('/', follow_redirects=True)
+        self.assertIn('registered?', result.data)
+
+    def test_user_without_session_redirect_homepage_to_login(self):
+        result = self.client.get('/', follow_redirects=True)
+        self.assertNotIn('Where are you?', result.data)
+
+    def test_user_without_session_no_logout_option(self):
+        result = self.client.get('/', follow_redirects=True)
+        self.assertNotIn('Log Out', result.data)
+
+
+class LoggedInTests(unittest.TestCase):
+    """Tests for registered users."""
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SECRET_KEY'] = 'key'
+        self.client = app.test_client()
+        connect_to_db(app, "postgresql:///testdb")
+        db.create_all()
+        example_data()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['user_id'] = 1
+
+    def tearDown(self):
+        db.session.close()
+        db.drop_all()
 
     def test_not_registered_yet(self):
-        result = self.client.get('/')
-        self.assertIn('registered?')
+        result = self.client.get('/', follow_redirects=True)
+        self.assertNotIn('registered?', result.data)
 
     def test_registered(self):
-        result = self.client.get('/')
-        self.assertNotIn('Where are you?')
+        result = self.client.get('/', follow_redirects=True)
+        self.assertIn('ready', result.data)
+
+    def test_visit_history(self):
+        result = self.client.get('/history')
+        self.assertIn('Where', result.data)
 
 
 class SeleniumTests(unittest.TestCase):
@@ -35,7 +69,7 @@ class SeleniumTests(unittest.TestCase):
         self.assertEqual(self.browser.title, 'Login')
 
     def test_homepage_title(self):
-        self.browser.get('http://localhost:5000/')
+        self.browser.get('http://localhost:5000')
         self.assertEqual(self.browser.title, 'Homepage')
 
 

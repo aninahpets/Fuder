4adadda0407780b1cad44a5f1555d2afa02b9d7e
Nina <smulhollandfuchs@gmail.com>
2016-08-17 17:32:32 -0700

    Completed MVP; ride request fully functioning, all helper functions moved to a single file

---
+++ helper_functions.py
@@ -0,0 +1,118 @@
+import os
+import pdb
+import googlemaps
+import requests
+from model import User, Venue, Visit, connect_to_db, db
+from random import randrange
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from uber_rides.auth import AuthorizationCodeGrant
+from uber_rides.client import UberRidesClient
+from uber_rides.session import OAuth2Credential
+from uber_rides.session import Session
+
+def get_start_coordinates(address):
+    """Geocode and return user's current location from input string."""
+
+    # Instantiate new Client object gmaps
+    gmaps = googlemaps.Client(key=os.environ['google_api_key'])
+
+    # Use gmaps object to geocode user input
+    geocode_result = gmaps.geocode('%s' % address)
+    coordinates = geocode_result[0]['geometry']['location']
+    start_latitude = coordinates['lat']
+    start_longitude = coordinates['lng']
+
+    return start_latitude, start_longitude
+
+
+def get_user_auth(uber_auth_flow):
+    """Send user to URL uber_auth_url to authorize app access."""
+    
+    uber_auth_url = uber_auth_flow.get_authorization_url()
+
+    return uber_auth_url
+
+
+def request_uber_ride(start_lat, start_lng, end_lat, end_lng, uber_auth_flow, code, state):
+    """Send a ride request on behalf of a user."""
+
+    # Instantiate new session & client object and retrieve credentials
+    uber_session = uber_auth_flow.get_session('http://0.0.0.0:5000/callback?code=%s&state=%s' % (code, state))
+    uber_client = UberRidesClient(uber_session, sandbox_mode=True)
+    credentials = uber_session.oauth2credential
+
+
+    response = uber_client.request_ride(
+        start_latitude=start_lat,
+        start_longitude=start_lng,
+        end_latitude=end_lat,
+        end_longitude=end_lng
+        )
+
+    ride_details = response.json
+    ride_id = ride_details.get('request_id')
+
+
+def search_yelp(start_lat, start_lng):
+    """
+    Use Yelp API v3 to fetch a venue; create venue and visit records in
+    database.
+    """
+   
+    # make a request to Yelp's oauth2/token endpoint using app credentials
+    resp = requests.post("https://api.yelp.com/oauth2/token",
+                         data={'grant_type': 'client_credentials',
+                               'client_id': os.environ['yelp_app_id'],
+                               'client_secret': os.environ['yelp_app_secret']})
+
+    # retrieve unique access token from Yelp's response
+    yelp_access_token = resp.json()['access_token']
+
+    # make a request to Yelp's API with the returned access token using the 
+    # start coordinates as search params
+    results = requests.get('https://api.yelp.com/v3/businesses/search?latitude=%s&longitude=%s&sort_by=rating&categories=restaurants&open_now_filter=True' % (start_lat, start_lng),
+        headers={'Authorization': 'Bearer %s' % yelp_access_token})
+
+    results = results.json()
+    
+    # select the business to which we will send the user at random
+    optionsnumber = randrange(len(results['businesses']))
+
+    # extract necessary data from json results and store in a dict
+    destination = {'name': results['businesses'][optionsnumber]['name'],
+        'id': results['businesses'][optionsnumber]['id'],
+        'latitude': results['businesses'][optionsnumber]['coordinates']['latitude'],
+        'longitude': results['businesses'][optionsnumber]['coordinates']['longitude']}
+
+
+    # check to see if the venue exists in the database (create a venue record
+    # if not) and create a visit record
+    match = db.session.query(Venue).filter_by(venue_id=destination['id']).first()
+
+    if match:
+        new_visit = Visit(user_id=session['user_id'],
+                            venue_id=destination['id'],
+                            start_lat=start_lat,
+                            start_lng=start_lng,
+                            end_lat=destination['latitude'],
+                            end_lng=destination['longitude'])
+        db.session.add(new_visit)
+        db.session.commit()
+
+    else:
+        new_venue = Venue(venue_id=destination['id'],
+                            name=destination['name'],
+                            latitude=destination['latitude'],
+                            longitude=destination['longitude'])
+        db.session.add(new_venue)
+        db.session.commit()
+
+        new_visit = Visit(user_id=session['user_id'],
+                            venue_id=destination['id'],
+                            start_lat=start_lat,
+                            start_lng=start_lng,
+                            end_lat=destination['latitude'],
+                            end_lng=destination['longitude'])
+        db.session.add(new_visit)
+        db.session.commit()
--- server.py
+++ server.py
@@ -2,14 +2,15 @@ import os
 import pdb
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
+from sqlalchemy import func, desc
 from model import User, Venue, Visit, connect_to_db, db
-from yelp_backend import *
-from uber_backend import *
+from helper_functions import *
 
 app = Flask(__name__)
-app.secret_key = "secretkeysecret"
+app.secret_key = "secret_key"
 
+# instantiating AuthorizationCodeGrant object for use with Uber methods
+# across the app
 uber_auth_flow = AuthorizationCodeGrant(
     os.environ['uber_client_id'],
     ['request'],
@@ -20,7 +21,7 @@ uber_auth_flow = AuthorizationCodeGrant(
 
 @app.route('/')
 def index():
-    """Homepage"""
+    """Checks for user login and returns homepage or login template."""
 
     # checks to see if user logged in; redirects to login if not
     if 'user_id' in session:
@@ -35,7 +36,7 @@ def index():
 
 @app.route('/login')
 def login():
-    """Provides user login form"""
+    """Provides user login form."""
 
     # checks to see if user logged in; redirect to homepage if so
     if 'user_id' in session:
@@ -44,9 +45,9 @@ def login():
         return render_template('login.html')
 
 
-
 @app.route('/login_submit', methods=['POST'])
 def submit_login():
+    """Logs user in to app."""
     # gets user email and pw from login form
     email = request.form.get('email')
     password = request.form.get('password')
@@ -56,7 +57,6 @@ def submit_login():
     if user == None:
         flash("Uh-oh, we couldn't find you. Please register.")
         return redirect('/login')
-    user_id = user.user_id
 
     # logs current user out if session exists
     if 'user_id' in session:
@@ -66,38 +66,35 @@ def submit_login():
     if user.password == password:
         session['user_id'] = user.user_id
         return redirect('/')
+
     # redirects to login page if unsuccessful
     else:
         flash('Your password was incorrect. Please try again.')
         return redirect('/login')
 
 
-
 @app.route('/register', methods=['POST'])
 def register():
+    """Registers user as a user of the app."""
     # TODO: Add error handling for duplicate email
     # TODO: Form validation
     email = request.form.get('email')
     password = request.form.get('password')
 
-    # create new user instance and add to database
+    # create new user record and add to database
     new_user = User(email=email, password=password)
     db.session.add(new_user)
     db.session.commit()
 
-    # retrieves newly created user object and creates new session
-    user = User.query.filter_by(email=email).one()
-    user_id = user.user_id
-    session['user_id'] = user.user_id
+    session['user_id'] = new_user.user_id
     return redirect('/')
-
 
 
 @app.route('/logout')
 def logout():
-    """Logs user out and removes session"""
+    """Logs user out of app."""
 
-    # removes user session and confirms user logout
+    # remove user session and confirm user logout
     del session['user_id']
     flash('You are now logged out.')
     return redirect('/login')
@@ -108,49 +105,60 @@ def logout():
 
 @app.route('/get_user_auth', methods=['POST'])
 def get_user_authorization():
-
-    # Retrieve user's location from text input
+    """
+    Retrieves user location, creates unique visit record, and redirects user
+    to Uber for authorization of app to make ride requests.
+    """
+    # retrieve user's location from text input and return as start coordinates
     user_location = request.form.get('user-address')
-
-    # geocode the user's location input
     start = get_start_coordinates(user_location)
 
-    # fetch destination venue from Yelp
+    # fetch destination venue from Yelp using start coordinates
+    # create a visit record in the database with start/end coordinates
     search_yelp(start[0], start[1])
 
+    # call get_user_auth, passing in uber_auth_flow object and redirect to
+    # custom auth URL provided by Uber
     url = get_user_auth(uber_auth_flow)
-
     return redirect(url)
-
 
 
 @app.route('/callback')
 def send_user_to_destination():
+    """
+    Retrieves credentials from Uber callback, retrieves start/end coordinates
+    from the database, and requests an Uber on behalf of the user.
+    """
 
-    # Retrieve code and state from Uber's GET request to /callback route
+    # retrieve code and state from Uber's GET request to /callback route
     code = request.args.get('code')
     state = request.args.get('state')
 
-    coordinates = db.session.query(Visit.start_lat, Visit.start_lng, Visit.end_lat, Visit.end_lng).filter_by(user_id=session['user_id']).first()
+    # retrieve start and end coordinates from newly created visit record
+    coordinates = db.session.query(Visit.start_lat, Visit.start_lng, Visit.end_lat, Visit.end_lng).filter_by(user_id=session['user_id']).order_by('visited_at desc').first()
 
+    # request a ride on behalf of the user
     request_uber_ride(coordinates[0], coordinates[1], coordinates[2], coordinates[3], uber_auth_flow, code, state)
     return render_template('waiting.html')
 
 
-
 @app.route('/history')
 def history():
-    """User view of their complete venue visit history"""
-    # add user visit query here
+    """Provides the user with a complete view of their visit history."""
+
     if 'user_id' in session:
-        visits = db.session.query(Venue.name,
-            Visit.visited_at).filter(User.user_id==session['user_id']).all()
- 
+        # ask in advance for all visit/venue data and add to list of 
+        # visits when there is a user match
+        visits = []
+        raw_visits = Visit.query.options(db.joinedload('venue')).all()
+        for visit in raw_visits:
+            if visit.user_id==session['user_id']:
+                visits.append(visit)
+        # pass list of visits to Jinja
         return render_template('visit-history.html', visits=visits)
 
     else:
         return render_template('login.html')
-
 
 
 if __name__ == '__main__':

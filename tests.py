7c7e345dadc985ab2e3d70935e5ea6bc9190f077
Nina <smulhollandfuchs@gmail.com>
2016-08-17 17:34:19 -0700

    Added Selenium tests and mocked get_start_coordinates function

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+from selenium import webdriver
 
 from server import app
 from model import db, connect_to_db
@@ -19,3 +20,33 @@ class HomepageTests(unittest.TestCase):
     def test_registered(self):
         result = self.client.get('/')
         self.assertNotIn('Where are you?')
+
+
+class SeleniumTests(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_login_title(self):
+        self.browser.get('http://localhost:5000/login')
+        self.assertEqual(self.browser.title, 'Login')
+
+    def test_homepage_title(self):
+        self.browser.get('http://localhost:5000/')
+        self.assertEqual(self.browser.title, 'Homepage')
+
+
+def _mock_get_start_coordinates(address):
+    latitude = 37.8044
+    longitude = 122.2711
+    return latitude, longitude
+
+import server
+server.get_start_coordinates = _mock_get_start_coordinates
+
+
+if __name__ == "__main__":
+    unittest.main()

211d5a76df92cb1c7bcc5e4c3c5e08b6b10dd6b2
Nina <smulhollandfuchs@gmail.com>
2016-08-19 13:16:26 -0700

    Adding AJAX and changing event listeners/handlers

--- static/registration-login-logout.js
+++ static/registration-login-logout.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
 
-$('#uber-auth-button').click(function() {
-        alert('Authorizing through Uber...');
-    }
-);
+function setUpForRide(evt) {
+    evt.preventDefault();
 
+    var formInputs = {'user-address': $('#field-user-address').val()};
+
+    $.post('/get_user_auth',
+            formInputs,
+            confirmRide
+            );
+}
+
+function confirmRide() {
+    alert('Your Uber is on its way');
+}
+
+$('#request-ride-button').click(setUpForRide);
 
 }
 )
-

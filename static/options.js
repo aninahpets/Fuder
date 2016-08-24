31df9244bafc291f7957dfaedb6d64073f5e3c16
Nina <smulhollandfuchs@gmail.com>
2016-08-24 14:43:04 -0700

    Fixed loop to add value as value rather than key as value

--- static/options.js
+++ static/options.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
 
-    // initialize the 'blank' dropdown
-    var initialDropdownState = $('#venue-options').html();
-
     function getOptions(evt) {
+        // initialize the 'blank' dropdown
+        var initialDropdownState = $('#venue-options').html();
         // set venueType to be the data value of the button clicked - 'bar' or 'restaurant'
         var venueType = $(this).data('venue-type');
         // make AJAX request using venueType to retrieve list of bar or restaurant options
@@ -17,13 +16,13 @@ $(document).ready(function () {
             for (key in results) {
                 var option = $('<option>');
                 // set the value of the tag to the val we will use for the API calls
-                option.attr({'value': results.key});
+                option.attr('value', results[key]);
                 // append the human-readable key to the variable option
                 option.append(key);
                 // append the variable option to venue-options
                 $('#venue-options').append(option);
             }
-    });
+        });
     }
 
     $('.venue-option-btn').click(getOptions);
